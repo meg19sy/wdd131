5819b4b23df7b01c3990a15ed7225cf7ac9e0e3e
meg19sy <161410501+meg19sy@users.noreply.github.com>
2024-06-07 21:12:53 +0800

    assignement for week 1

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+// JavaScript to dynamically populate the current year
+document.getElementById('currentyear').textContent = new Date().getFullYear();
+
+// JavaScript to populate the last modified date
+document.getElementById('lastModified').textContent = 'Last Modified: ' + document.lastModified;
